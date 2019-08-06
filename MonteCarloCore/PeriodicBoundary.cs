b9d2f06715180c5fbfcd32e7adc7cc6100482631
Laura Herzog <herzogla93@gmail.com>
2019-08-06 11:07:50 -0400

    Added code to periodic boundary condition class

--- MonteCarloCore/PeriodicBoundary.cs
+++ MonteCarloCore/PeriodicBoundary.cs
@@ -6,7 +6,33 @@ using System.Threading.Tasks;
 
 namespace MonteCarloCore
 {
-    class PeriodicBoundary
+    public class PeriodicBoundary : Boundary
     {
+        public PeriodicBoundary(Position myPosition) : base(myPosition)
+        {
+        }
+
+        public override bool CheckBoundaryCondition(SimulationBox box, SimulationObject mcObject)
+        {
+            if (BoxPosition == Position.Bottom)
+            {
+                mcObject.Y -= box.Height;
+            }
+
+            if (BoxPosition == Position.Top)
+            {
+                mcObject.Y += box.Height;
+            }
+
+            if (BoxPosition == Position.Right)
+            {
+                mcObject.X -= box.Width;
+            }
+
+            if (BoxPosition == Position.Left)
+            {
+                mcObject.X += box.Width;
+            }
+        }
     }
 }
