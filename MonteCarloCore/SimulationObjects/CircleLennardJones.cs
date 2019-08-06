97198f734737efc936b555c032b2781f52286a6f
user <user@XRD-DATABASE>
2019-08-06 16:40:22 -0400

    updated potential (fixed)

--- MonteCarloCore/SimulationObjects/CircleLennardJones.cs
+++ MonteCarloCore/SimulationObjects/CircleLennardJones.cs
@@ -5,17 +5,28 @@ namespace MonteCarloCore
     public class CircleLennardJones : Circle
 
     {
-     
+        public double wellDepth = 1;
+        
         public override double CalculateInteractionEnergy(SimulationObject Obj)
         {
             // lennard-jones potential
             if (Obj is Circle circle)
             {
-                double rm = circle.Radius + Radius;
-                double r = Math.Sqrt((circle.X - X) * (circle.X - X)
-                    + (circle.Y - Y) * (circle.Y - Y));
-                double eps = 1;
-                double potential = eps * (Math.Pow((rm / r), 12.0) - 2.0 * Math.Pow((rm / r), 6.0));
+                double potential;
+                if (circle.Radius + Radius
+                     < Math.Sqrt((circle.X - X) * (circle.X - X)
+                     + (circle.Y - Y) * (circle.Y - Y)))
+                {
+                    double rm = circle.Radius + Radius;
+                    double r = Math.Sqrt((circle.X - X) * (circle.X - X)
+                        + (circle.Y - Y) * (circle.Y - Y));
+                    potential = wellDepth * (Math.Pow((rm / r), 12.0) - 2.0 * Math.Pow((rm / r), 6.0));
+                }
+                else
+                {
+                    //kick it out
+                    potential = 10000000;
+                }
                 return potential;
             }
             else
