e7376052634348d83fbb805b800bc73c74335aa4
rztum <rztumbleson@gmail.com>
2019-08-06 10:40:51 -0400

    hardwall boundary condition added

---
+++ MonteCarloCore/HardWall.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonteCarloCore
+{
+    public class HardWall : Boundary
+    {
+        public override bool CheckBoundaryCondition(SimulationBox box, SimulationObject mcObject)
+        {
+            if ( BoxPosition == Position.Top || BoxPosition == Position.Bottom)
+            {
+                mcObject.Y = mcObject.PreviousY;
+            }
+            if (BoxPosition == Position.Left || BoxPosition == Position.Right)
+            {
+                mcObject.X = mcObject.PreviousX;
+            }
+            return true;
+        }
+
+        protected HardWall(Position myPosition) : base(myPosition)
+        {
+
+        }
+        public Position BoxPosition;
+    }
+}
